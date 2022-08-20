73d90d97264898977dad752e66449e6b0f6a8aac
Lucidize-Jad <adamu.jaafaru2020@uniabuja.edu.ng>
2022-08-20 02:44:28 -0700

    First Commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="Header-wrapper">
+      <h2>Project Management Board</h2>
+    </div>
+  )
+}
+
+export default Header;
